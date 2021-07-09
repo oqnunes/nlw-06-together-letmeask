f86f1d05fb509f0b57d801540da875641dcff8bb
Development <development@MBP-de-Macbook.home>
2021-07-09 14:45:52 +0100

    New(): Created the Application Route Structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
 import { NewSpace } from "./pages/NewSpace";
 
 function App() {
   return (
-    <NewSpace />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/new-space" component={NewSpace} />
+    </BrowserRouter>
   );
 }
 
